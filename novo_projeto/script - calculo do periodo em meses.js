a78900b6b74c771627254b20db651abc12207646
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-21 11:38:37 -0300

    att

--- novo_projeto/script - calculo do periodo em meses.js
+++ novo_projeto/script - calculo do periodo em meses.js
@@ -6,7 +6,7 @@ function myFunction() {
     var data2 = new Date(dataFinal);
 
     if(data1 > data2){
-        alert('A data em que você começou a trabalhar deve ser maior que a data do último dia')
+        alert('A data em que você começou a trabalhar deve ser menor que a data do último dia')
     }
 
     var total = (data2.getFullYear() - data1.getFullYear())*12 + (data2.getMonth() - data1.getMonth());
